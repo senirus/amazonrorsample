b453d711d42eeced491540ec7fe172ca555d37dc
senirus1-admin <seniru.s@circles.asia>
2023-09-12 17:57:15 +0530

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  resources :microposts
-  resources :users
-  root 'users#index'
+  root 'static_pages#home'  
+  get  'static_pages/home'
+  get  'static_pages/help'
+  get  'static_pages/about'
 end
