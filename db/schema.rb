1d410048195122f8d9f7f1d742d0dd4e6eed3894
senirus1-admin <seniru.s@circles.asia>
2023-09-22 14:04:01 +0530

    Add account activation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_21_065343) do
+ActiveRecord::Schema.define(version: 2023_09_22_064805) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 2023_09_21_065343) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
