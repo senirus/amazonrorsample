65db77887f7813041cb6626239ac90cc1eab9981
senirus1-admin <seniru.s@circles.asia>
2023-10-02 09:39:37 +0530

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ User.create!(name: "Example User",
              password:              "foobar",
              password_confirmation: "foobar",
              admin:    true,
-            #  activated: true,
-            #  activated_at: Time.zone.now
+             activated: true,
+             activated_at: Time.zone.now
             )
     
 # Generate a bunch of additional users.
@@ -27,4 +27,12 @@ users = User.order(:created_at).take(6)
     content = Faker::Lorem.sentence(word_count: 5)
     users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Create following relationships.
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+# following.each { |followed| user.follow(followed) }
+# followers.each { |follower| follower.follow(user) }
 end
